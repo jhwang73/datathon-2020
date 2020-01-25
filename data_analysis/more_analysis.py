b589dcd2e5e7c5e65c713a4d32ba1c3bad2fe865
Alex Xiong <awx1@rice.edu>
2020-01-25 14:17:49 -0600

    LEJLSG

--- data_analysis/more_analysis.py
+++ data_analysis/more_analysis.py
@@ -28,19 +28,47 @@ zc_total_count = pd.read_csv(zc_total_count_plus)
 zc_total_count.set_index("ZIP", inplace=True)
 print(zc_total_count)
 
+
+pop_raw = '/Users/alexanderxiong/Documents/GitHub/datathon-2020/data/population_zip_harris.csv'
+pop = pd.read_csv(pop_raw)
+pop.set_index("zip_code", inplace=True)
+
+zc_cat_raw = '/Users/alexanderxiong/Documents/GitHub/datathon-2020/data/sumCrimeCountByCategorizedCrime_Zipcode.csv'
+zc_cat = pd.read_csv(zc_cat_raw)
+zc_cat.set_index("zipcode", inplace=True)
+
+# for grange,zcs in gentLevel_dict.items():
+# 	popCount = 0
+# 	for zc in val:
+# 		popCount += zip_pop.loc[zc]['PopCount']
+
+# 	crimecategory_avgcountpercapita = {}
+# 	for category in crime_categories:
+# 		for zc in val:
+# 			category_crime_count += zip_crime.loc[zc]['CrimeCount']['auto']
+# 		crimecategory_avgcountpercapita[category] = category_crime_count / popCount
+
+
+
+
+
 zip_pop_file = '/Users/alexanderxiong/Documents/GitHub/datathon-2020/data/ZipcodeToCrimeCountBy100.csv'
 zip_pop = pd.read_csv(zip_pop_file)
 zip_pop.set_index("zipcode", inplace=True)
 crime_dict = {0:0, 1:0, 2:0, 3:0, 4:0}
 
 for grange, zcs in gentLevel_dict.items():
+	pop_count = 0
 	total_crime_count = 0
 	valid_count = 0
 	for zc in zcs:
 		if (zc in zc_total_count.index.values):
 			total_crime_count += zc_total_count.loc[zc].tolist()[0]
+			
+			pop_count += pop.loc[zc]["y-2016"]
+
 			valid_count += 1
-	crime_dict[grange] = round(total_crime_count / valid_count, 2)
+	crime_dict[grange] = round(total_crime_count / (valid_count * pop_count), 2)
 
 # for key,val in gentLevel_dict.items():
 # 	crimeCount = 0
@@ -55,6 +83,10 @@ with open('gentLevelToPerCapitaDrime.csv', 'w') as f:
 	for key in crime_dict.keys():
 		f.write("%s,%s\n"%(key, crime_dict[key]))
 
+
+
+
+
 # crime_file = '/Users/alexanderxiong/Documents/GitHub/datathon-2020/data/NIBRSPublicView.Jan1-Dec31-FINAL.xlsx'
 # crime = pd.read_excel(crime_file)
 
@@ -66,16 +98,4 @@ with open('gentLevelToPerCapitaDrime.csv', 'w') as f:
 # with open('differentCrimes.csv', 'w', newline='') as myfile:
 # 	wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 # 	wr.writerow(comp_charges)
-
-# for key,val in gentLevel_dict.items():
-# 	crime 
-# 	popCount = 0
-# 	for zc in val:
-# 		popCount += zip_pop.loc[zc]['PopCount']
-
-# 	crimecategory_avgcountpercapita = {}
-# 	for category in crime_categories:
-# 		for zc in val:
-# 			category_crime_count += zip_crime.loc[zc]['CrimeCount']['auto']
-# 		crimecategory_avgcountpercapita[category] = category_crime_count / popCount
 
